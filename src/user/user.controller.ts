4ceee7a6f265088478353005e78fa2ff6e4e4e39
Goran <pavlovskigoran88@gmail.com>
2021-03-16 23:10:02 +0100

    create users service and connect to db

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './models/user.entity';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Get()
-  all() {
-    return ['users'];
+  async all(): Promise<User[]> {
+    return await this.userService.all();
   }
 }
