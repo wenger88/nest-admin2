4e74bfe7f84c44176dc116ea05af4b795529ab1e
Goran <pavlovskigoran88@gmail.com>
2021-03-16 23:20:21 +0100

    create auth controller and register users method

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,4 +12,8 @@ export class UserService {
   async all(): Promise<User[]> {
     return await this.userRepository.find();
   }
+
+  async create(data): Promise<User> {
+    return this.userRepository.save(data);
+  }
 }
