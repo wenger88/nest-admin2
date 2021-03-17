f5d7dcc9db7489e8ea9f3aa75ed608c91f6aa581
Goran <pavlovskigoran88@gmail.com>
2021-03-17 21:01:45 +0100

    implement jwt for the login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,20 @@
-import { BadRequestException, Body, Controller, NotFoundException, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
 import { RegisterDto } from './models/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Controller()
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private jwtService: JwtService) {}
 
   @Post('register')
   async register(@Body() body: RegisterDto) {
@@ -21,6 +30,7 @@ export class AuthController {
   async login(
     @Body('email') email: string,
     @Body('password') password: string,
+    @Res({ passthrough: true }) response: Response
   ) {
     const user = await this.userService.findOne({ email });
     if (!user) {
@@ -29,6 +39,11 @@ export class AuthController {
     if (!(await bcrypt.compare(password, user.password))) {
       throw new BadRequestException('Invalid credentials!');
     }
+
+    const jwt = await this.jwtService.signAsync({ id: user.id });
+
+    response.cookie('jwt', jwt, { httpOnly: true });
+
     return user;
   }
 }
