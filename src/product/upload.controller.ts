24224b241a191af1c8e0f2cda4fd49e9db246e47
Goran <pavlovskigoran88@gmail.com>
2021-03-21 21:23:54 +0100

    image upload and get image

---
+++ src/product/upload.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { Response } from 'express';
+
+@Controller()
+export class UploadController {
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename(
+          req,
+          file: Express.Multer.File,
+          callback: (error: Error | null, filename: string) => void
+        ) {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          return callback(null, `${randomName}${extname(file.originalname)}`);
+        },
+      }),
+    })
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      url: `http://localhost:8000/api/${file.path}`,
+    };
+  }
+
+  @Get('uploads/:path')
+  async getImage(@Param('path') path, @Res() res: Response) {
+    res.sendFile(path, { root: 'uploads' });
+  }
+}
