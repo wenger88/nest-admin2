e269e209c4b2158f57316db1c6c1f011d7cdd6cb
Goran <pavlovskigoran88@gmail.com>
2021-03-29 23:32:10 +0200

    cors fix 2

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  app.enableCors({
-    origin: 'https://nest-be.herokuapp.com/', //change this value based on the project localhost usage
-    credentials: true,
-  });
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
